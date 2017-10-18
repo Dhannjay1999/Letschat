89d4cc551b6035e2df01605d323b36c625828b44
Dhannjay1999 <dhannjay.vohra@gmail.com>
2017-10-18 17:53:39 +0530

    Seventh Commit

---
+++ app/src/main/java/com/example/lenovo/letschat/Details.java
@@ -0,0 +1,21 @@
+package com.example.lenovo.letschat;
+
+/**
+ * Created by Lenovo on 10/17/2017.
+ */
+
+public class Details {
+    private  String name;
+public Details(){
+
+}
+    public Details(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
